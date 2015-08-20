b7e542420e6e629fe95dc5b514034855ba62f779
sharadau <sharada.umarane@gmail.com>
2015-08-20 12:36:15 +0530

    added new core configurations

--- scripts/services/search.js
+++ scripts/services/search.js
@@ -98,11 +98,11 @@ angular.module('dashboardApp')
                 success: function (callback) {successCallback = callback; return response;},
                 error: function (callback) {errorCallback = callback; return response;}
             };
-           // http://localhost:8983/solr/new_core1/select?q=City%3APune&wt=json&indent=true
 
             var catArray = category_name.split(',');
-           // alert(records);
-            //alert(service_base_url + '/select?q=City%3A%22' + category_name + '%22&start=' + start + '&rows=' + records + '&wt=json&indent=true&hl=true&hl.fl=JobDescription+JobTitle+WholePosting&hl.simple.pre=<em>&hl.simple.post=<%2Fem>');
+            category_name = category_name.replace(/ /g, "+");
+            category_name = category_name.replace(/&/g, "%26");
+           // alert(service_base_url + '/select?q=CompanyName%3A%22' + category_name + '%22&start=' + start + '&rows=' + records + '&wt=json&indent=true&hl=true&hl.fl=JobDescription+JobTitle+WholePosting&hl.simple.pre=<em>&hl.simple.post=<%2Fem>');
             //$http.get(service_base_url+'/select?q=City%3A%22'+category_name+'%22&start='+start+'&rows='+records+'&wt=json&indent=true&hl=true&hl.fl=JobDescription+JobTitle+WholePosting&hl.simple.pre=<em>&hl.simple.post=<%2Fem>')
             $http.get(service_base_url+'/select?q=CompanyName%3A%22'+category_name+'%22&start='+start+'&rows='+records+'&wt=json&indent=true&hl=true&hl.fl=JobDescription+JobTitle+WholePosting&hl.simple.pre=<em>&hl.simple.post=<%2Fem>')
                 .success(function(item){
@@ -160,8 +160,8 @@ angular.module('dashboardApp')
             {
                 limitCondition = '&facet.limit='+limit;
             }
-
-            $http.get(service_base_url+'/select?q=*%3A*&fl=City&wt=json&indent=true&facet=true&facet.field=CompanyName'+limitCondition)
+            alert(service_base_url+'/select?q=*%3A*&fl=CompanyName&wt=json&indent=true&facet=true&facet.field=CompanyName'+limitCondition);
+            $http.get(service_base_url+'/select?q=*%3A*&fl=CompanyName&wt=json&indent=true&facet=true&facet.field=CompanyName'+limitCondition)
                 .success(function(item){
                     successCallback(item);
                 })
